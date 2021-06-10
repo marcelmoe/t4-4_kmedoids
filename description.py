2d6c1d4dd25d9a66adf9072e633494f4adf7e79d
Beschroe <benedikt.schroeter308@gmail.com>
2021-06-10 17:57:42 +0200

    fix:  Pep8 adaptions and correction of a small type

--- description.py
+++ description.py
@@ -1,40 +1,43 @@
 """
-This module is meant to describe the data sets analysed in the deployed app
+This module is meant to describe the data sets analysed in the deployed app.
+It is not involved in the app itself.
 """
 
 import pandas as pd
+
 
 def data_clearance():
     """
     takes raw data and
     :return: modifed data frames used for further analysis
     """
-    #Import data
+    # Import data
     df_red_wine = pd.read_csv("data_sets\winequality-red.csv", delimiter=";")
     # Adjust lowest value to 0
     df_red_wine["quality"] = df_red_wine["quality"] - 3
     # Bring classification column "quality" to front of data frame
     df_red_wine = pd.concat([df_red_wine["quality"],
-                                  df_red_wine.drop(["quality"], axis=1)], axis=1)
+                             df_red_wine.drop(["quality"], axis=1)], axis=1)
 
-    #Introduce column names
+    # Introduce column names
     headers = ["Class", "Alcohol", "Malic acid", "Ash", "Alcalinity of ash", "Magnesium",
                "Total phenols", "Flavanoids", "Nonflavanoid phenols", "Proanthocyanins",
                "Color intensity", "Hue", "OD280/OD315 of diluted wines", "Proline"]
-    #Import data
+    # Import data
     df_wine = pd.read_csv("data_sets\wine.csv", header=None, names=headers)
     # Adjust values from column 'Class'
     df_wine["Class"] = df_wine["Class"] - 1
 
-    #Introduce column names
-    headers=['sepal length in cm', 'sepal width in cm', 'petal length in cm',
-             'petal width in cm', 'class']
-    #Import data
+    # Introduce column names
+    headers = ['sepal length in cm', 'sepal width in cm', 'petal length in cm',
+               'petal width in cm', 'class']
+    # Import data
     df_iris = pd.read_csv("data_sets\iris.csv", header=None, names=headers)
     # Bring classification column 'class' to front of data frame
     df_iris = pd.concat([df_iris["class"], df_iris.drop(["class"], axis=1)], axis=1)
 
     return df_red_wine, df_wine, df_iris
+
 
 def data_description(df_red_wine, df_wine, df_iris):
     """
@@ -42,14 +45,15 @@ def data_description(df_red_wine, df_wine, df_iris):
     :return: None
     """
     red_describe = df_red_wine.apply(pd.DataFrame.describe, axis=1)
-    with open('Descriptin_red.tex','w') as tf:
+    with open('Descriptin_red.tex', 'w') as tf:
         tf.write(red_describe.to_latex())
     wine_describe = df_wine.apply(pd.DataFrame.describe, axis=1)
-    with open('Description_wine.tex','w') as tf:
+    with open('Description_wine.tex', 'w') as tf:
         tf.write(wine_describe.to_latex())
-    iris_describe = df_wine.apply(pd.DataFrame.describe, axis=1)
-    with open('Description_iris.tex','w') as tf:
+    iris_describe = df_iris.apply(pd.DataFrame.describe, axis=1)
+    with open('Description_iris.tex', 'w') as tf:
         tf.write(iris_describe.to_latex())
+
 
 def main():
     """
@@ -62,4 +66,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
