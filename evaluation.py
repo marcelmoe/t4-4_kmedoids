0af39258b5ab2a7618bbbc24af89da276c47dd86
Beschroe <benedikt.schroeter308@gmail.com>
2021-06-10 17:58:24 +0200

    feat: Faster implementation of the silhouette coefficient using sklearn.

--- evaluation.py
+++ evaluation.py
@@ -4,6 +4,7 @@ The evaluation module provides several functions to measure clustering quality.
 
 import numpy as np
 from sklearn.metrics import rand_score
+from sklearn.metrics import silhouette_samples
 
 
 def sum_of_squares(df, cluster_count):
@@ -20,12 +21,11 @@ def sum_of_squares(df, cluster_count):
 
     # Iterate over all clusters
     for i in range(0, cluster_count):
-
         # sub dataframe containing observations assigned to cluster i
         df_i = df.loc[df[df.columns[0]] == i]
 
         # sum of squares computation
-        ssq = (((df_i - df_i.mean())**2).to_numpy()).sum()
+        ssq = (((df_i - df_i.mean()) ** 2).to_numpy()).sum()
 
         result = result + [ssq]
 
@@ -33,11 +33,53 @@ def sum_of_squares(df, cluster_count):
 
 
 def rand_index(labels_true, labels_pred):
+    """
+    Rand Index:
+    In verbal terms, the index measures the proportion of pairs of elements that both clusterings place in the same or
+    different clusters.  Accordingly, the index has a value between 0 and 1.  The closer the value is to 1, the more
+    similar the compared clusterings are
+    :param labels_true: Dataframe or numpy array containing the true labels.
+    :param labels_pred: Dataframe or numpy array containing the predicted labels.
+    :return: Rand Score
+    """
     return rand_score(labels_true, labels_pred)
 
 
 def silhouette_coefficient(df, cluster_count):
     """
+    Silhouette Coefficient:
+    It is an internal measure for the quality of a cluster.
+    For each observation i it is defined as:
+    s(i) = (Separation(i)-Cohesion(i))/max(Separation(i),Cohesion(i))
+    Separation(i) is defined as the smallest mean distance of observation i to all observations of any other cluster
+    Cohesion(i) is defined as the mean distance of observation i to all observations in the same cluster as i.
+    For a complete cluster the Silhouette Coefficient is defined as the average over all silhouette coefficients of
+    its observations.
+    :param df: Pandas dataframe. It is assumed that df's first column contains the observations cluster-label. It
+    is further assumed that the class labels range from 0 to (cluster_count - 1)
+    :param cluster_count: Number of clusters/classes.
+    :return: It returns a tuple where the first entry is a numpy array containing the silhouette coefficients for all
+     clusters of df and the second entry the over all silhouette coefficient for all observations
+    """
+    # Computes the silhouette value for each sample
+    sk_sh = silhouette_samples(df.drop([df.columns[0]], axis=1), df[df.columns[0]])
+    # Prepare the output list
+    result = []
+    # Iterate over all clusters
+    for i in range(cluster_count):
+        # We sum over all silhouette values from samples assigned to the i-th cluster
+        # Then, we divide by the number of those samples and append to the output list
+        indices = df.index[df[df.columns[0]] == i].tolist()
+        result = result + [sum(sk_sh[indices]) / len(sk_sh[indices])]
+
+    return result
+
+
+def silhouette_coefficient_deprecated(df, cluster_count):
+    """
+    Hint: This is an own implementation of the silhouette coefficient.
+    The alternative implementation, which is essentially based on that of sklearn, should be preferred as it is
+    significantly faster.
     Silhouette Coefficient:
     It is an internal measure for the quality of a cluster.
     For each observation i it is defined as:
@@ -74,7 +116,7 @@ def silhouette_coefficient(df, cluster_count):
             dist = (np.linalg.norm(cluster_i_np - cluster_i_np[j], axis=1)).sum()
 
             # Take the average
-            avg_dist = dist/(len(cluster_i_np)-1)
+            avg_dist = dist / (len(cluster_i_np) - 1)
 
             # The cohesion is equal to the average distance
             cohesion_j = avg_dist
